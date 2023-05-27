319bd91f46215f6f65997bce5a709df52c963869
Nick Brian <nbjm1234@outlook.com>
2023-05-27 03:27:15 -0600

    busqueda dinamica por cliente terminada

--- controllers-V2/inputDynamicSearchByNameController/inputDynamicByNameController.js
+++ controllers-V2/inputDynamicSearchByNameController/inputDynamicByNameController.js
@@ -4,11 +4,22 @@ const {productStock}= require("../../models-v2")
 
 const getProductByName= async (req,res) => {
   try {
-    const {name} = req.params
-    const data= (await productStock.find({})).filter((item) => {
-      return item.name.includes(name)
+    const {client, productName} = req.body
+
+    const find = await productStock.findOne({client})
+    const clientStock= find.productsInStock
+    const productFound = clientStock.filter((item)=> {
+      return item.name.includes(productName)
     })
-    res.send({data})
+    // const data= (await productStock.findOne({client})).filter((item) => {
+
+    //   return item.productsInStock.name.includes(productName)
+    // })
+    if (productFound.length===0){
+      return  res.status(404).json({ error: 'producto no encontrado' })
+    }else {
+      res.send({data:productFound})
+    }
   } catch (error) {
     
   }
